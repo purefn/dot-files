9914998854fa5e60b14dd14131bbe3a8c992952e
Richard Wallace <rwallace@thewallacepack.net>
2011-07-11 14:57:51 -0700

    fix the setup script to work properly

--- setup.sh
+++ setup.sh
@@ -14,28 +14,43 @@ resolve_links() {
   echo -n $L
 }
 
-make_dir_fully_qualified() {
+make_fully_qualified() {
   saveddir=`pwd`
-  D=`cd "$1" && pwd`
+  if [ -f $1 ]
+  then
+    echo -n `cd $(dirname "$1") && pwd`/`basename "$1"`
+  else
+    echo -n `cd "$1" && pwd`
+  fi
+
   cd $saveddir
-  echo -n $D
 }
 
-DOT=`make_dir_fully_qualified "$(dirname "$(resolve_links "$0")")/dot"`
+DOT=`make_fully_qualified "$(dirname "$(resolve_links "$0")")/dot"`
 
 for f in $DOT/*
 do
   DST="$HOME/."`basename $f`
   if [ -e "$DST" ]
   then
-    if [ "-h \"$DST\"" -a "`resolve_links \"$DST\"` -eq \"$DST.bck\"" ]
+    if [ -h "$DST" ]
     then
-      echo Skipping already setup "$DST"
+      if [ "$(make_fully_qualified "$(resolve_links "$DST")")" = "$f" ]
+      then
+        echo Skipping already setup "$DST"
+      else
+        echo Making backup and linking $DST
+        mv "$DST" "$DST.bck"
+        ln -sf "$f" "$DST"
+      fi
     else
+      echo Making backup and linking $DST
       mv "$DST" "$DST.bck"
+      ln -sf "$f" "$DST"
     fi
+  else
+    echo Linking $DST
+    ln -sf "$f" "$DST"
   fi
-
-  ln -sf "$f" "$DST"
 done
 

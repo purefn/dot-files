2e7ee2c3ecd3a523ff724e03e58f890f7a00c398
Richard Wallace <rwallace@thewallacepack.net>
2015-10-29 13:37:08 -0700

    ignore only the configuration.nix link

---
+++ nixos/machines/MacPro3,1/configuration.nix
@@ -0,0 +1,43 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/audio.nix
+      ../../modules/desktop.nix
+      ../../modules/networking.nix
+      ../../modules/services.nix
+      ../../modules/system.nix
+    ];
+
+  # Use the gummiboot efi boot loader.
+  boot = {
+    kernelParams = [ "i8042.noaux" "i8042.nokdb" "i8042.nopnp" ];
+
+    loader = {
+      gummiboot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking.hostName = "ronin";
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      linuxPackages = pkgs.linuxPackages_latest;
+    };
+  };
+
+  powerManagement.enable = false;
+
+  services = {
+    mediatomb.enable = true;
+
+    xserver.videoDrivers = [ "nvidiaLegacy340" ];
+  };
+}
---
+++ nixos/machines/Precision-M4700/configuration.nix
@@ -0,0 +1,27 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./machines/dell-laptop/hardware-configuration.nix
+      ./modules/audio.nix
+      ./modules/desktop.nix
+      ./modules/laptop.nix
+      ./modules/networking.nix
+      ./modules/services.nix
+      ./modules/system.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    device = "/dev/sda";
+  };
+
+  networking.hostName = "tealc";
+}

9bce1d761a148d55c82beef27c15cdefd3ed0f7d
Richard Wallace <rwallace@thewallacepack.net>
2015-11-04 12:17:24 -0700

    a few changes

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -4,7 +4,8 @@
   networking = {
     extraHosts = ''
       192.168.0.10 ronin
-      192.168.0.4 tealc
+      192.168.0.11 tealc
+      192.168.0.12 seedbox
       68.3.236.160 habitat
     '';
     networkmanager.enable = true;

f4a0afaf00e8a58b6b95fe21b3b584406d281ba6
Richard Wallace <rwallace@thewallacepack.net>
2022-02-09 14:58:59 -0700

    sops

--- modules/sops/default.nix
+++ modules/sops/default.nix
@@ -2,8 +2,7 @@
   sops = {
     defaultSopsFile = ./secrets.yaml;
 
-    # sops.age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
-    # sops.age.keyFile = "/var/lib/sops-nix/key.txt";
-    # sops.age.generateKey = true;
+    # age.keyFile = "/var/lib/sops-nix/key.txt";
+    # age.generateKey = true;
   };
 }

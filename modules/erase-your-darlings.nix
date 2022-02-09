de6ed06c8a4f27b0842d116fab93b46f9686b7fa
Richard Wallace <rwallace@thewallacepack.net>
2022-02-09 14:10:59 -0700

    wha

--- modules/erase-your-darlings.nix
+++ modules/erase-your-darlings.nix
@@ -67,8 +67,6 @@
   users = {
     mutableUsers = false;
     users = {
-      # root.initialHashedPassword = "$6$gdpMMTVeVitX0$jbXCRI/yWr6AzsL2K2VyPvNApb0xb8iipCkv2SPCALf3dz9vRjKlPRUFVWgd2OOA7ZJeRs8sFSNL0rd072fHG.";
-      # rwallace.initialHashedPassword = "$6$caUyXsJ6$YqTfq2glYOMpbmONsO1iVXmmJjlOIDHNp9EATJeLApity2Bf6nAqrsoFmFS/Mb9qMLMyLNVEMMHVGJk4Zx4Bp.";
       root.passwordFile = config.sops.secrets."passwords/root".path;
       rwallace.passwordFile = config.sops.secrets."passwords/rwallace".path;
     };

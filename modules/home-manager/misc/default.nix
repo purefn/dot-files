3087dea1067b3c6b0889e1a5fe6db7ac755e521a
Richard Wallace <rwallace@thewallacepack.net>
2022-05-12 16:40:37 -0700

    update secrets

--- modules/home-manager/misc/default.nix
+++ modules/home-manager/misc/default.nix
@@ -5,7 +5,7 @@
   home = {
     packages = with pkgs; [
       nix-prefetch-git
-      sshfsFuse
+      sshfs-fuse
     ];
 
     # why not authorized_keys file here? it needs to be in a directory that is not
@@ -13,12 +13,10 @@
     file = {
       ".ssh/c-deploy.pub".source = ./ssh/c-deploy.pub;
       ".ssh/id_ed25519.pub".source = ./ssh/id_ed25519.pub;
-      ".ssh/id_ed25519_w.pub".source = ./ssh/id_ed25519_w.pub;
       ".ssh/id_rsa.pub".source = ./ssh/id_rsa.pub;
 
       ".ssh/c-deploy".source = config.lib.file.mkOutOfStoreSymlink nixos-config.sops.secrets."ssh/c_deploy".path;
       ".ssh/id_ed25519".source = config.lib.file.mkOutOfStoreSymlink nixos-config.sops.secrets."ssh/ed".path;
-      ".ssh/id_ed25519_w".source = config.lib.file.mkOutOfStoreSymlink nixos-config.sops.secrets."ssh/ed_w".path;
       ".ssh/id_rsa".source = config.lib.file.mkOutOfStoreSymlink nixos-config.sops.secrets."ssh/rsa".path;
     };
   };

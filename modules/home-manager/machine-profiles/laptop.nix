e4b7def958255c9389c615eb6681d75183b37251
Richard Wallace <rwallace@thewallacepack.net>
2022-10-17 10:13:55 -0700

    cleanup

--- modules/home-manager/machine-profiles/laptop.nix
+++ modules/home-manager/machine-profiles/laptop.nix
@@ -7,24 +7,4 @@
     # notification messages are still useful
     cbatticon.enable = true;
   };
-
-  systemd.user.services.socks5-proxy-gut = {
-    Service = {
-      ExecStart="${pkgs.openssh}/bin/ssh -D 8889 -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -CN gut";
-      Restart = "always";
-      RestartSec = 5;
-    };
-
-    Install = { WantedBy = [ "default.target" ]; };
-  };
-
-  systemd.user.services.socks5-proxy-tealc = {
-    Service = {
-      ExecStart="${pkgs.openssh}/bin/ssh -D 8080 -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -CN tealc";
-      Restart = "always";
-      RestartSec = 5;
-    };
-
-    Install = { WantedBy = [ "default.target" ]; };
-  };
 }
